4da04d62559641ea90034860b17586c9431f4fdd
edubragabonfim <eduardo.bonfim.dub@gmail.com>
2025-08-23 22:16:39 -0300

    Conclusão da primeira parte do exercício 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,66 @@
+﻿//4) Crie um programa que embaralha ("criptografa") uma string usando o seguinte algoritmo:
+//a string é reescrita em blocos de 5 caracteres, sendo as novas palavras obtidas lendo-se cada coluna resultante,
+//separadas por asteriscos. Por exemplo, se a string for "mensagem secreta":
+
+//mensa
+//gem s
+//ecret
+//a
+
+//A criptografia retorna "mgea*eec *nmr *s e *ast *" (atenção para os espaços em branco).
+//O programa deve, também, ser capaz de receber uma string criptografada com o método descrito e retornar
+//uma string "simples" com a mensagem original.
+
+using System.Text;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static string Criptografia(string mensagem)
+        {
+            int tamanho = mensagem.Length;
+            int linhas = tamanho / 5;
+            int acrescimo = tamanho % 5 > 0 ? 1 : 0;
+            char[] vtr_mensagem = mensagem.ToCharArray();
+            int contador = 0;
+
+            StringBuilder sb = new StringBuilder();
+
+            char[,] mtz_mensagem = new char[linhas + acrescimo + 1, 5];
+
+            for (int i = 0; i < mtz_mensagem.GetLength(0); i++)
+            {
+                for (int j = 0; j < mtz_mensagem.GetLength(1); j++)
+                {
+                    if (i == mtz_mensagem.GetLength(0) - 1)
+                        mtz_mensagem[i, j] = '*';
+                    else
+                        if (contador < vtr_mensagem.Length)
+                            mtz_mensagem[i, j] = vtr_mensagem[contador];
+                        else
+                            mtz_mensagem[i, j] = ' ';
+                    contador++;
+                }
+            }
+
+            for (int j = 0; j < mtz_mensagem.GetLength(1); j++)
+            {
+                for (int i = 0; i < mtz_mensagem.GetLength(0); i++)
+                {
+                    sb.Append(mtz_mensagem[i,j]);
+                }
+            }
+
+
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            // "mgea*eec *nmr *s e *ast *
+            string retorno = Criptografia("mensagem secreta");
+            Console.WriteLine(retorno);         
+        }
+    }
+}
